978bf814dedf4a07e070cd32b8d2a9508121a84f
umitotuki <umitukiuk@gmail.com>
2024-04-11 07:18:45 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      @books = Book.all
        flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = "not created correctly"
       @books = Book.all
       render :index
     end
@@ -29,14 +27,11 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      @books = Book.all
      flash[:notice] = "Book was successfully updated."
      redirect_to book_path(@book.id)
-   else
-     flash.now[:alert] = "not update"
-     @books = Book.all
+    else
      render :edit
-   end
+    end
   end
   
   def destroy
